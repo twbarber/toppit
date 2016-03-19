b7e534e546394b7fb625fb8d1508ced7cfff9c5c
Tyler Barber <tylerwbarber@gmail.com>
2016-03-19 17:26:22 -0500

    Added setup, removed idea

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='toppit',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/twbarber/toppit',
+    license='',
+    author='Tyler Barber',
+    author_email='tylerwbarber@gmail.com',
+    description='Receive emails for top subreddit posts.',
+    install_requires=[
+          'praw'
+    ]
+)
