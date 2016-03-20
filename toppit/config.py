d9b368f73a6c1ef5105a8b1136f034e2464cabe0
Tyler Barber <tylerwbarber@gmail.com>
2016-03-20 11:54:29 -0500

    Interval and count configurability started. ohgodthisisgross. Config needs to be a class. In too much of a hurry. Need coffee, will fix there.

--- toppit/config.py
+++ toppit/config.py
@@ -1,16 +1,57 @@
 import configparser
 
+conf = None
+
+
+def get_sender_email():
+    global conf
+    return conf.get('toppit', 'Email')
+
+
+def get_sender_password():
+    global conf
+    return conf.get('toppit', 'Password')
+
+
+def get_recipients():
+    global conf
+    recipients = []
+    for recipient in conf.get('toppit', 'Recipients').split(','):
+        recipients.append(recipient)
+    return recipients
+
+
+def get_subreddits():
+    global conf
+    subreddits = []
+    for subreddit in conf.get('toppit', 'Subreddits').split(','):
+        subreddits.append(subreddit)
+    return subreddits
+
+
+def get_interval():
+    global conf
+    return conf.get('toppit', 'Interval')
+
+
+def get_count():
+    global conf
+    return conf.get('toppit', 'Count')
+
 
 def load_config():
+    global conf
     config = configparser.ConfigParser()
-    config.read('.toppit')
-    subreddits = []
-    for subreddit in config.get('toppit', 'Subreddits').split(','):
-        subreddits.append(subreddit)
-    recipients = []
-    for recipient in config.get('toppit', 'Recipients').split(','):
-        recipients.append(recipient)
-    return dict(email=config.get('toppit', 'Email'),
-                password=config.get('toppit', 'Password'),
+    conf = config.read('.toppit')
+    email = get_sender_email()
+    password = get_sender_password()
+    subreddits = get_subreddits()
+    recipients = get_recipients()
+    interval = get_interval()
+    count = get_count()
+    return dict(email=email,
+                password=password,
                 subreddits=subreddits,
-                recipients=recipients)
+                recipients=recipients,
+                interval=interval,
+                count=count)
