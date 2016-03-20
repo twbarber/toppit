78e0b68f0048959949e09f7354714fb2cbd077a2
Tyler Barber <tylerwbarber@gmail.com>
2016-03-20 11:14:36 -0500

    Clarified example config, included license in setup.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='toppit',
-    version='0.1',
-    packages=[''],
+    version='0.1.0',
+    packages=['toppit'],
     url='https://github.com/twbarber/toppit',
-    license='',
+    license='MIT',
     author='Tyler Barber',
     author_email='tylerwbarber@gmail.com',
     description='Receive emails for top subreddit posts.',
